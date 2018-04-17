adaa544e704a9696ef39e92146652967bfdb787e
mdrachonov <mykhailo.drachonov@thecoded.co.uk>
2018-04-17 09:27:35 +0200

    TRAIN-167 przerobione

--- public_html/newjavascript.js
+++ public_html/newjavascript.js
@@ -1,58 +1,69 @@
 class Timer {
-    countDown() {
-        this.s--;
+    
+    constructor(id1, id2) {
+        self = this;
         
-        if (this.s < 0) { //sprawdza sekundy na dodatność
-            this.s = 59;
-            this.m--;
+        self.idStart = document.getElementById(id1);
+        self.idReset = document.getElementById(id2);
+        
+        self.idStart.onclick = self.start;
+        self.idReset.onclick = self.resetTimer;
+    }
+    
+    countDown() {
+        self.s--;
+        
+        if (self.s < 0) {
+            self.s = 59;
+            self.m--;
         }
         
-        if (this.m < 0) { //sprawdza minuty na dodatność
-            this.m = 59;
+        if (self.m < 0) {
+            self.m = 59;
         } 
         
-        if (this.s + this.m == 0) resetTimer();  //resetuje przy końcu odlicznia
-        
-        this.s = this.s + "";
-        this.m = this.m + "";
-        
-        if (this.s.length < 2) { 
-            this.s = "0" + this.s; //robi dwucyfrową liczbę sekund
+        if (self.s + self.m == 0) {
+            self.resetTimer();
         }
         
-        if (this.m.length < 2) { 
-            this.m = "0" + this.m; //robi dwucyfrową liczbę minut
+        self.s = self.s + "";
+        self.m = self.m + "";
+        
+        if (self.s.length < 2) { 
+            self.s = "0" + self.s;
         }
         
-        document.getElementById('tm').innerHTML = this.m + ":" + this.s; 
+        if (self.m.length < 2) { 
+            self.m = "0" + self.m;
+        }
+        
+        document.getElementById("tm").innerHTML = self.m + ":" + self.s; 
     }    
        
     resetTimer(){             
-            clearInterval(this.timerId);
+            clearInterval(self.timerId);
             
-            this.m = 0;
-            this.s = 0;
+            self.m = 0;
+            self.s = 0;
             
-            document.getElementById('tm').innerHTML = '00' + ':' + '00';
+            document.getElementById("tm").innerHTML = '00' + ':' + '00';
             document.getElementById("minutes").value = "";
-            document.getElementById("minutes").removeAttribute("disabled","");
+            document.getElementById("minutes").removeAttribute("disabled", "");
             
-            this.status = 0;
+            self.status = 0;
         }
    
     start(){   
-        document.getElementById("minutes").setAttribute("disabled","");
+        document.getElementById("minutes").setAttribute("disabled", "");
         
-        this.m = +document.getElementById("minutes").value; //ustaliamy zmienne
-        this.s = 0;
-        this.status = 0; 
-        this.timerId;
+        self.m = +document.getElementById("minutes").value;
+        self.s = 0;
+        self.status = 0; 
+        self.timerId;
           
-        if(!this.status){       //sprawdza status minutnika
-            this.status = 1;
-            this.timerId = setInterval(this.countDown.bind(this), 1000); //uruchamia minutnik
+        if(!self.status){
+            self.status = 1;
+            self.timerId = setInterval(self.countDown.bind(self), 1000);
         }
     }
 }
-
-var timer = new Timer();
