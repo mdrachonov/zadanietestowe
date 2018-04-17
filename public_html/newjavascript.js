771bb0ae203b244a7b254de7dd5ed7e84aac73e1
mdrachonov <mykhailo.drachonov@thecoded.co.uk>
2018-04-17 07:16:05 +0200

    TRAIN-166 przerobione

--- public_html/newjavascript.js
+++ public_html/newjavascript.js
@@ -1,26 +1,29 @@
 class startTime {
-    constructor(){
-            this.count();
+    
+    constructor() {
+        this.count();
     }
-    count(){
-            var date = new Date();
+    
+    count() {
+        var date = new Date();
 
-            this.h = date.getHours();
-            this.m = date.getMinutes(); 
-            this.s = date.getSeconds();
+        this.h = date.getHours();
+        this.m = date.getMinutes(); 
+        this.s = date.getSeconds();
 
-            this.m = this.checkTime(this.m); 
-            this.s = this.checkTime(this.s); 
+        this.m = this.checkTime(this.m); 
+        this.s = this.checkTime(this.s); 
 
-            document.getElementById('clock').innerHTML = this.h + ":" + this.m + ":" + this.s; 
+        document.getElementById('clock').innerHTML = this.h + ":" + this.m + ":" + this.s; 
 
-            this.t = setTimeout(this.count.bind(this), 500);
+        this.t = setTimeout(this.count.bind(this), 500);
     }
 
     checkTime(i) {
-            if (i < 10) i = "0" + i;
+        if (i < 10) {
+            i = "0" + i;
+        }
 
-            return i;
+        return i;
     }
 }
-var Clock = new startTime();
