fe873e30325c92e8e3ddea857a5fb7a8a37fa24f
mdrachonov <mykhailo.drachonov@thecoded.co.uk>
2018-07-05 14:47:24 +0200

    TRAIN-185

--- public_html/script.js
+++ public_html/script.js
@@ -1,51 +1,55 @@
-jQuery(document).ready(function() {
-    var minlen = 1;
-    var keyint = 50;
-    var term = '';
-    var n = 0;
-    var time_keyup = 0;
-    var time_search = 0;
-    
-    function dosearch() {
-        term = jQuery('#finder').val();
+class Finder {
+    constructor() {
+        this.minlen = 1;
+        this.keyint = 50;
+        this.term = '';
+        this.n = 0;
+        this.time_keyup = 0;
+        this.time_search = 0;
+        self = this;
+    }
+
+    dosearch() {
+        self.term = jQuery('#finder').val();
 
         jQuery('span.highlight').each(function() {
             jQuery(this).after(jQuery(this).html()).remove();  
         });
 
         jQuery('div.content').each(function() {
-            jQuery(this).html(jQuery(this).html().replace(new RegExp(term, 'ig'), '<span class="highlight">$&</span>'));
+            jQuery(this).html(jQuery(this).html().replace(new RegExp("(?![^<>]*>)(" + self.term + ")", 'ig'), '<span class="highlight">$&</span>'));
 
-            n = jQuery('span.highlight').length;
+            this.n = jQuery('span.highlight').length;
 
-            if (n == 0) {
+            if (this.n == 0) {
                 jQuery('#find-result').html('Nie ma wyników');
             } else {
-                jQuery('#find-result').html('Wyników: ' + n); 
+                jQuery('#find-result').html('Wyników: ' + this.n); 
             }
         });
     }
-   
-    jQuery('#finder').keyup(function() {
-        var d1 = new Date();
-        time_keyup = d1.getTime();
 
-        if (jQuery('#finder').val() != term) {
-            if (jQuery('#finder').val().length >= minlen) {
+    startFind() {
+        this.d1 = new Date();
+        self.time_keyup = this.d1.getTime();
+
+        if (jQuery('#finder').val() != self.term) {
+            if (jQuery('#finder').val().length >= self.minlen) {
                 setTimeout(function() {
-                    var d2 = new Date();
-                    time_search = d2.getTime();
+                    this.d2 = new Date();
+                    self.time_search = this.d2.getTime();
     
-                    if (time_search - time_keyup >= keyint) {
-                        dosearch();
+                    if (self.time_search - self.time_keyup >= self.keyint) {
+                        self.dosearch();
                     }
-                }, keyint); 
+                }, self.keyint); 
             } else {
                 jQuery('#find-result').html('&nbsp');
+
                 jQuery('span.highlight').each(function() {
                     jQuery(this).after(jQuery(this).html()).remove();  
                 });
             }
         }
-    });	
-});
+    }
+}
